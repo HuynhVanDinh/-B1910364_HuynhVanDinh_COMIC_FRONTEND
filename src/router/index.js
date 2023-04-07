509bdb590ca0b21d49942a6b34b52e435401698d
HuynhVanDinh <dinh0375751606@gmail.com>
2023-04-07 12:54:47 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,11 @@ const routes = [
     name: "comic",
     component: Comic,
   },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
+  },
 ];
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
